8de1e69b0f7c613333c3ccca18c0082829f0d70d
Shawn Weigand <shawnweigand1@gmail.com>
2025-05-02 08:15:37 -0400

    feat: :sparkles: chunk endpoint api

--- routes/api.php
+++ routes/api.php
@@ -8,3 +8,9 @@ Route::get('/user', function (Request $request) {
         'user' => 'johnny test'
     ];
 });
+
+Route::post('/chunk', function (Request $request) {
+    return [
+        'request' => $request->all(),
+    ];
+});
